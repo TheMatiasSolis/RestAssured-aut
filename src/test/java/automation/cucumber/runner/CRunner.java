974317d0ad5bad91c7a4d73ff9d93e3fc347ba3b
Matias Solis <msolisjara@gmail.com>
2026-06-30 21:27:43 -0400

    se limpian dependencias

--- src/test/java/automation/cucumber/runner/CRunner.java
+++ src/test/java/automation/cucumber/runner/CRunner.java
@@ -1,24 +1,23 @@
 package automation.cucumber.runner;
 
-import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/automation/cucumber/features/PetStore",
-        glue = {"automation.cucumber.hook", "automation.cucumber.steps"},
+        glue = {
+                "automation.cucumber.hook",
+                "automation.cucumber.steps"
+        },
         plugin = {
                 "pretty",
                 "html:target/cucumber",
                 "json:target/cucumber.json",
-                "junit:build/test-results/test/xml/ejecucion.xml"
+                "junit:build/test-results/test/ejecucion.xml"
         },
         tags = "@CrearPet"
 )
-
 public class CRunner extends AbstractTestNGCucumberTests {
 
     @Override
@@ -26,6 +25,4 @@ public class CRunner extends AbstractTestNGCucumberTests {
     public Object[][] scenarios() {
         return super.scenarios();
     }
-
 }
-
